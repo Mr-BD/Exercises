456e188acc11dc19f590d173de510428dbb6c9b0
Mr-BD <52127297+Mr-BD@users.noreply.github.com>
2023-02-10 15:39:06 +0200

    new changes

--- Java/src/ArraySum.java
+++ Java/src/ArraySum.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class ArraySum {
     public static void main(String[] args) {
-        int arr[] = new int[] {12,2,3,5,10,4};
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum+=arr[i];
-        }
-        System.out.println("The Sum is: "+sum);
+      Scanner sc = new Scanner(System.in);
+      int day = sc.nextInt();
+      switch (day){
+          case 1:
+              System.out.println("Monday");
+              break;
+
+      }
+
+
+        System.out.println("day");
 
     }
 }
--- Java/src/Matrix1.java
+++ Java/src/Matrix1.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 
 public class Matrix1 {
     public static void main(String[] args){
-        int arr[][] = { { 2, 7, 9, 20 }, { 30, 6, 1, 50 }, { 70, 45, 22, 67 } };
+        int arr[][] = { { 2, 7, 9, 20 },
+                         { 30, 6, 1, 50 },
+                         { 70, 45, 22, 67 } };
         // printing 2D array
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++)
